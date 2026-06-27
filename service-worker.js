32a3aba73206d4de1517ec539f2a8cb49be0bbcb
Kamal Fikri Nabawi <kamalfikrinabawi@gmail.com>
2026-06-27 09:51:15 +0700

    create app

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "putting-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png",
+    "./fonts/Inter-Regular.otf",
+    "./fonts/BebasNeue-Regular.ttf",
+    "./fonts/JetBrainsMono-Regular.ttf"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
